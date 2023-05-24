5dfbfcf70787b364d2bf05e60617ad5af104f87a
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2023-05-24 10:50:47 +0630

    Initial Commit

---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by Nyi Ye Han on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
